0d570f5f7ebeb023873cc629d502e2da8c21c2e9
Evan Relf <evan@evanrelf.com>
2015-12-28 23:32:08 -0800

    Change everything

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
-duckduckgoog
-============
+# Local DuckDuckGoog
 
-Use DuckDuckGo for !bangs and Google for everything else
+> Use DuckDuckGo's powerful [!bangs](https://duckduckgo.com/bang) without sacrificing Google's search results, with the speed and privacy of a locally-hosted server!
+
+Instead of sending your searches to a remote server, a local web server is started on your machine when you login to respond to search queries instantaneously.
+
+Instead of sending searches to a slow remote server, a local web server handles requests instantaneously. This improves loading times in your browser, and keeps your searches private.
+
+This is a stripped-down fork of [@mikecrittenden's original project](https://github.com/mikecrittenden/duckduckgoog), without the user-facing webpages.
+
+Because I'm using Mac OS X's `launchd` to start the web server on login, this unfortunately doesn't work on Windows or Linux. In the future support could be added using `cron` jobs or something.
+
+## Installation
+
+Make sure you have Git, Node, and NPM installed.
+
+**ATTENTION:** Review the [install script](install) before running this command---you should never trust random people on the internet.
+
+```bash
+curl -fL https://raw.githubusercontent.com/evanrelf/duckduckgoog/master/install | bash
+```
+
+If you aren't comfortable pasting that in, just follow the commands in the install script manually.
+
+## Usage
+
+Searches containing a DuckDuckGo bang (e.g. `!wiki George Washington`) are sent to DuckDuckGo, otherwise they are sent to Google.
--- app.js
+++ app.js
@@ -1,72 +1,35 @@
-var express = require('express'),
-    http = require('http');
+var express = require('express');
+var http = require('http');
 
 var app = express();
 
-app.configure(function(){
+app.configure(function() {
   app.set('port', process.env.PORT || 3000);
-  app.set('view engine', 'jade');
-  app.use(express.favicon(__dirname + '/public/images/favicon.ico'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
+  app.set('searchEngine', process.env.SEARCH_ENGINE || 'https://www.google.com/search?q=');
   app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
-
-app.configure('development', function(){
-  app.use(express.errorHandler());
-  app.use(express.logger('dev'));
-});
-
-app.configure('production', function(){
-  app.use(express.errorHandler());
-  app.use(express.logger());
 });
 
 app.get('/', function(req, res) {
-  if (req.query["q"]) {
-    query = req.query["q"];
-    encQuery = encodeURIComponent(query);
+  query = req.query['q'];
+  encodedQuery = encodeURIComponent(query);
 
-    if (query.match(/![A-Za-z0-9]+/) || query.substring(0, 2) === "! " || query.substring(0, 1) === "\\") {
+  if (query.match(/![A-Za-z0-9]+/) ||
+      query.substring(0, 2) === '! ' ||
+      query.substring(0, 1) === '\\') {
+    console.log('Searched DuckDuckGo');
+    res.redirect('https://duckduckgo.com?q=' + encodedQuery);
 
-      console.log('Queried DuckDuckGo');
-      res.redirect('https://duckduckgo.com?q=' + encQuery);
-
-    } else if (req.query['searchengine']) {
-
-      console.log('Queried custom search engine');
-      searchEngine = req.query['searchengine'];
-      if (searchEngine.search() != -1 && (searchEngine.lastIndexOf('http://', 0) === 0 || searchEngine.lastIndexOf('https://', 0) === 0)) {
-        customSearchURL = searchEngine.replace(/%q/g, encQuery);
-        res.redirect(customSearchURL);
-      } else {
-        console.log('Error in search engine syntax. Using Google.');
-        res.redirect('https://www.google.com/search?q=' + encQuery);
-      }
-
+  } else {
+    if (app.get('searchEngine').indexOf('google.com') != -1) {
+      console.log('Searched Google');
     } else {
-
-      console.log('Queried Google');
-      if (req.query['google']) {
-        res.redirect('https://' + req.query['google'] + '/search?q=' + encQuery);
-      } else {
-        res.redirect('https://www.google.com/search?q=' + encQuery);
-      }
-
+      console.log('Searched custom');
     }
+
+    res.redirect(app.get('searchEngine') + encodedQuery);
   }
-  res.render('index');
 });
 
-app.get('/browser', function(req, res) {
-  res.render('browser');
-});
-
-app.get('/privacy', function(req, res) {
-  res.render('privacy');
-});
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+http.createServer(app).listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
 });
